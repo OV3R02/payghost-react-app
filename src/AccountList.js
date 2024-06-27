e649e272fedf97a1c0d4bc29b21a00e1ce526d93
Omar Vergnano <omarverg37@proton.me>
2024-06-27 14:20:47 +0200

    commit5

--- src/AccountList.js
+++ src/AccountList.js
@@ -1,21 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function AccountList() {
     const [accounts, setAccounts] = useState([]);
 
-    const tk = sessionStorage.getItem("token");
-    fetch('http://localhost:8080/payghost/api/accounts',
-        {
-            method: "GET",
-            headers: {
-                'Content-Type':'application/json',
-                'Authorization':`Bearer ${tk}`
-            }
-        })
-        .then(res => res.json())
-        .then(json => {
-            arrayJson = json;
-        })
-        .catch()
+    useEffect(() => {
 
+        const tk = sessionStorage.getItem("token");
+        fetch('http://localhost:8080/payghost/api/accounts',
+            {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${tk}`
+                }
+            })
+            .then(res => res.json())
+            .then(json => {
+                setAccounts(json);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+
+
+    },[accounts])
+
+    return(
+        <>
+        
+        <select className='form-select'>
+            {accounts.map((curac) => <OptionAccount ac={curac}/>)}
+        </select>
+        
+        </>
+    )
 }
+
+function OptionAccount(props) {
+    return <option account-id = {props.ac.id}>{props.ac.fname}{props.ac.lname}</option>; 
+}
+export default AccountList;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import Register from './Register';
 import Login from './Login';
+import AccountList from './AccountList';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,7 +13,7 @@ root.render(
     <App />
     <Register />
     <Login />
-    
+    <AccountList />
     
   </React.StrictMode>
 );
