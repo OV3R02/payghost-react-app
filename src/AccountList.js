035c9f9b03145c63afd7011ff5918aa1d75d644c
Omar Vergnano <omarverg37@proton.me>
2024-07-05 09:32:17 +0200

    commit

--- src/AccountList.js
+++ src/AccountList.js
@@ -1,14 +1,11 @@
-import { useEffect, useState } from "react";
-
-function AccountList() {
+import { useState, useEffect } from "react";
+function Accountlist() {
     const [accounts, setAccounts] = useState([]);
-
     useEffect(() => {
-
         const tk = sessionStorage.getItem("token");
         fetch('http://localhost:8080/payghost/api/accounts',
             {
-                method: "GET",
+                method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
                     'Authorization': `Bearer ${tk}`
@@ -19,24 +16,27 @@ function AccountList() {
                 setAccounts(json);
             })
             .catch((error) => {
+                const json = [{ id: 22, fname: "qwe", lname: "wer" }, { id: 33, fname: "asd", lname: "asd" }]
                 console.log(error);
-            })
-
-
-    },[accounts])
-
-    return(
+                setAccounts(json);
+            }
+            );
+    }, [accounts]);
+    return (
         <>
-        
-        <select className='form-select'>
-            {accounts.map((curac) => <OptionAccount ac={curac}/>)}
-        </select>
-        
+            <select className="form-select">
+                {accounts.map((curac) => <OptionAccount ac={curac} />)}
+
+            </select>
+
+
         </>
     )
+
 }
 
 function OptionAccount(props) {
-    return <option account-id = {props.ac.id}>{props.ac.fname}{props.ac.lname}</option>; 
+    return <option account-id={props.ac.id} value={props.ac.id}>{props.ac.fname} {props.ac.lname}</option>;
 }
-export default AccountList;
+
+export default Accountlist
--- src/Register.js
+++ src/Register.js
@@ -1,98 +1,68 @@
 import { useState } from "react";
-
-
-function Register(){
-    
+import './Register.css';
+function Register() {
     const [fname, setFname] = useState("");
     const [lname, setLname] = useState("");
     const [email, setEmail] = useState("");
     const [pwd, setPwd] = useState("");
     const [confirmPwd, setConfirmPwd] = useState("");
     const [credit, setCredit] = useState("");
-    
-    
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        const dati = {
-            fname, 
-            lname, 
-            email, 
-            pwd, 
+        const dati ={
+            fname,
+            lname,
+            email,
+            pwd,
             confirmPwd,
             credit
-        };
-        const stringDati = JSON.stringify(dati);
+        }
+        ;
+        const stringDati=JSON.stringify(dati);
 
         fetch('http://localhost:8080/payghost/api/accounts',
-          {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: stringDati
-          })
-          .then(res => res.text())
-          .then(testo => {
-            console.log(testo);
-            alert(`id nuovo=${testo}`);
-    
-          })
-          .catch((error) => {
-            console.log(error);
-          }
-          );
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: stringDati
+            })
+        .then(res => res.text())
+        .then(testo => {
+                console.log(testo);
+                alert(`id nuovo=${testo}`);
+
+            })
+        .catch((error) => {
+                console.log(error);
+              }
+            );
     }
-  
+
     return (
-      <form onSubmit={handleSubmit}>
-        <label>Enter your firstname:
-          <input 
-            type="text" 
-            value={fname}
-            onChange={(e) => setFname(e.target.value)}
-          />
-        </label>
-        <label>Enter your lastname:
-          <input 
-            type="text" 
-            value={lname}
-            onChange={(e) => setLname(e.target.value)}
-          />
-        </label>
-        <label>Enter your email:
-          <input 
-            type="text" 
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <label>Enter your password:
-          <input 
-            type="text" 
-            value={pwd}
-            onChange={(e) => setPwd(e.target.value)}
-          />
-        </label>
-        <label>Enter again your password:
-          <input 
-            type="text" 
-            value={confirmPwd}
-            onChange={(e) => setConfirmPwd(e.target.value)}
-          />
-        </label>
-        <label>Enter again your password:
-          <input 
-            type="text" 
-            value={credit}
-            onChange={(e) => setCredit(e.target.value)}
-          />
-        </label>
-        
-        
-        
-        
-        <input type="submit" />
-      </form>
+        <form onSubmit={handleSubmit}>
+            <label>Enter your firstname:
+                <input type="text" value={fname} onChange={(e) => setFname(e.target.value)} />
+            </label>
+            <label>Enter your lastname:
+                <input type="text" value={lname} onChange={(e) => setLname(e.target.value)} />
+            </label>
+            <label>Enter your email:
+                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+            </label>
+            <label>Enter your pwd:
+                <input type="text" value={pwd} onChange={(e) => setPwd(e.target.value)} />
+            </label>
+            <label>Enter your confirmPwd:
+                <input type="text" value={confirmPwd} onChange={(e) => setConfirmPwd(e.target.value)} />
+            </label>
+            <label>Enter your credit:
+                <input type="text" value={credit} onChange={(e) => setCredit(e.target.value)} />
+            </label>
+            <input type="submit" />
+        </form>
     )
+
 }
 
-export default Register;
+export default Register
